5635d8445de4e07de187437436af6011b7e9fcff
khatwanimohit <mohitkhatwani@google.com>
2024-05-30 14:19:39 +0000

    Llama2 forward pass checker

--- end_to_end/tpu/llama2/70b/2_test_llama2_70b.sh
+++ end_to_end/tpu/llama2/70b/2_test_llama2_70b.sh
@@ -59,3 +59,6 @@ python MaxText/generate_param_only_checkpoint.py MaxText/configs/base.yml base_o
 
 # Now, run decoding on the checkpoint generated from our finetune run.
 python MaxText/decode.py MaxText/configs/base.yml tokenizer_path=assets/tokenizer.llama2 load_parameters_path=${BASE_OUTPUT_PATH}/${PARAM_RUN_NAME}/checkpoints/0/items per_device_batch_size=1 run_name=runner_$(date +%Y-%m-%d-%H-%M) max_prefill_predict_length=4 max_target_length=16 dataset_type=synthetic steps=10 async_checkpointing=false scan_layers=false model_name=${MODEL_VARIATION} attention=dot_product prompt="I love to"
+
+# We also test whether the forward pass logits match the golden logits for Llama2-70b
+python3 MaxText/tests/forward_pass_logit_checker.py  MaxText/configs/base.yml base_output_directory=${BASE_OUTPUT_PATH} load_parameters_path=${UNSCANNED_CKPT_PATH} run_name=forward_pass_test per_device_batch_size=1 model_name=llama2-70b ici_tensor_parallelism=4 max_prefill_predict_length=4 max_target_length=4 dataset_type=synthetic dtype=float32 scan_layers=false async_checkpointing=false
